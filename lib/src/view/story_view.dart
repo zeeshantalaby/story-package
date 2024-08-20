f8e317fe745a46b84e4948f9840304b335b4385c
zeeshantalaby <132256275+zeeshantalaby@users.noreply.github.com>
2024-08-20 19:17:39 +0400

    Update story_view.dart => adding minus

--- lib/src/view/story_view.dart
+++ lib/src/view/story_view.dart
@@ -59,9 +59,9 @@ class _StoryViewState extends State<StoryView> {
           );
         },
         child: GestureDetector(
-          // onHorizontalDragUpdate: _handleDragUpdate,
-          // onHorizontalDragEnd: _handleDragEnd,
-          // onHorizontalDragCancel: _resetParams,
+          onHorizontalDragUpdate: _handleDragUpdate,
+          onHorizontalDragEnd: _handleDragEnd,
+          onHorizontalDragCancel: _resetParams,
           child: PageView.builder(
             allowImplicitScrolling: _provider!.preloadStory,
             physics: const NeverScrollableScrollPhysics(),
@@ -113,7 +113,7 @@ class _StoryViewState extends State<StoryView> {
 
   void _handleDragUpdate(DragUpdateDetails details) {
     if (_isAnimating) return;
-    final delta = details.primaryDelta!;
+    final delta = -details.primaryDelta!;
     final cont = _provider!.controller;
     final pageCont = cont.storyController!;
 
