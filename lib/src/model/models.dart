d513cf58e3e581bbd8fc1c297b678397a4eb7a12
zeeshantalaby <132256275+zeeshantalaby@users.noreply.github.com>
2024-08-21 20:15:34 +0400

    Delete lib directory

--- lib/src/model/models.dart
+++
@@ -1,8 +0,0 @@
-export '../contants/enums.dart' show StoryEvent;
-export 'story.dart';
-export 'story_position.dart';
-export 'style/advstory_style.dart';
-export 'style/indicator_style.dart';
-export 'style/shimmer_style.dart';
-export 'style/loading_style.dart';
-export 'style/tray_list_style.dart';
